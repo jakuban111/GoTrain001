325cf7ff109c14b649c60a4274152bd208d98a02
jakuban111 <jakubrozyna97@gmail.com>
2021-12-24 10:49:14 +0100

    you

--- hello1.go
+++ hello1.go
@@ -4,4 +4,13 @@ import "fmt"
 
 func main() {
 	fmt.Println("hello world")
+	fun1()
+	for i := 0; i < 100; i++ {
+		if i%2 == 0 {
+			fmt.Print(i)
+		}
+	}
+}
+func fun1() {
+	fmt.Print("działanie funkcji")
 }
