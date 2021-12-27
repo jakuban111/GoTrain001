26fa92222be8e2e138a01a68750137a56c6695b5
jakuban111 <jakubrozyna97@gmail.com>
2021-12-27 16:31:13 +0100

    you

--- hello1.go
+++ hello1.go
@@ -3,6 +3,9 @@ package main
 import "fmt"
 
 func main() {
+	ninja_1_3()
+}
+func ninja_1_1() {
 	x := 42
 	y := "James Bond"
 	z := true
@@ -10,4 +13,22 @@ func main() {
 	fmt.Println(y)
 	fmt.Println(z)
 	fmt.Println(x, y, z)
+}
+func ninja_1_2() {
+	var x int
+	var y string
+	var z bool
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+}
+func ninja_1_3() {
+	x := 42
+	y := "James Bond"
+	z := true
+	var s string
+	s = fmt.Sprintf("%v\t%v\t%v", x, y, z)
+	fmt.Printf(s)
+	//s := fmt.Sprintf(x, y, z)
+	//fmt.Println(s)
 }
